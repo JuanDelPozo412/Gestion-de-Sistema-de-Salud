432c53b4527de7e90bdd620724b23bad83a4476f
Juan Del Pozo <juandelpozo412@gmail.com>
2025-04-16 14:03:52 -0300

    Historial
    Primer commit de historial

---
+++ Github repository/src/Historial.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Historial {
+    private Paciente paciente;
+    private LinkedList<Turno>personas = new ArrayList<>();
+
+    public Historial(Paciente paciente, LinkedList<Turno> personas) {
+        this.paciente = paciente;
+        this.personas = personas;
+    }
+
+    public LinkedList<Turno> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(LinkedList<Turno> personas) {
+        this.personas = personas;
+    }
+
+    @Override
+    public String toString() {
+        return "Historial{" +
+                "paciente=" + paciente +
+                ", personas=" + personas +
+                '}';
+    }
+}
