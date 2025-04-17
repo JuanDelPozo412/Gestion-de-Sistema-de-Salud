a9c4c9d65443474a856b3912980f9452339e507c
FerFernandez28 <ferchufm560@gmail.com>
2025-04-17 14:46:59 -0300

    se agrego atributos. constructor, getters-setter y funcion login

--- Github repository/src/Persona.java
+++ Github repository/src/Persona.java
@@ -1,3 +1,67 @@
+import javax.swing.*;
+import java.time.LocalDate;
+
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private String contrasenia;
+    private LocalDate fechaNacimiento;
+
+    public Persona(String nombre, String apellido, String mail, String contrasenia, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.contrasenia = contrasenia;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void login(String mailIngresado, String contraseniaIngresada) {
+        if (mailIngresado.equals(mail) && contraseniaIngresada.equals(contrasenia)){
+            JOptionPane.showMessageDialog(null, "Puede Ingresar :)");
+        }else{
+            JOptionPane.showMessageDialog(null, "No puede ingresar :( ");
+        }
+    }
 
 }
