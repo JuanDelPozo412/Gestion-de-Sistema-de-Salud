04e829ad6c1f40e3a2d85849ff152cf01d9d37a7
Florb11 <florbergman11@gmail.com>
2025-04-21 18:00:19 -0300

    commit

--- Github repository/src/Main.java
+++ Github repository/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //JOptionPane.showMessageDialog(null, "Hola Mundo");
         //JOptionPane.showMessageDialog(null, "Hola Mundo 2");
-        Paciente paciente1 = new Paciente("flor","bergman","f@gmail.com","123", LocalDate.now(),"fem");
-       paciente1.login("f@gmail.com","123");
+        Paciente paciente1 = new Paciente("flor","bergman","@gmail.com",3435,"123",LocalDate.now(),"sexo");
+       paciente1.login("@gmail.com","123");
     }
 }
