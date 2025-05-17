ef20a63c70a1ab2e1bb81f3ba3eee948f1562dff
Juan Del Pozo <juandelpozo412@gmail.com>
2025-05-17 18:08:38 -0300

    login

--- Github repository/src/Main.java
+++ Github repository/src/Main.java
@@ -3,6 +3,12 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
+        String nombre = JOptionPane.showInputDialog(null,"Ingresa tu nombre de usuario");
+        String contrasenia = JOptionPane.showInputDialog(null,"Ingresa tu contrasenia");
+
+        Usuario.login(nombre,contrasenia);
+
         //JOptionPane.showMessageDialog(null, "Hola Mundo");
         //JOptionPane.showMessageDialog(null, "Hola Mundo 2");
         //Paciente paciente1 = new Paciente("flor","bergman","@gmail.com",3435,"123",LocalDate.now(),"sexo");
@@ -15,39 +21,39 @@ public class Main {
 
        //Medico medico1 = new Medico("Fer", "Fernandez", "@gmail", 43335197, "1234", LocalDate.of(2004,03,03), Especialidades.CLINICO, 5335);
 
-            String[] opciones = new String[]{"Menu paciente", "Menu medicos","Menu Administrador","Menu Personal Administrativo","Salir"};
-            int opcion;
-            do {
-                opcion = JOptionPane.showOptionDialog(null,
-                        "Seleccione una opcion:",
-                        "Menu principal",
-                        0,
-                        0,
-                        null,
-                        opciones,
-                        opciones);
-                switch (opcion) {
-                    case 0:
-                        paciente1.menuPaciente();
-                        break;
-                    case 1:
-                        medico1.menuMedico();
-                        break;
-                    case 2:
-                        admin.menuadministrador();
-                        break;
-                    case 3:
-                        personal.menuPersonalAdministrativo();
-                        break;
-                    case 4:
-                        JOptionPane.showMessageDialog(null, "Saliendo...");
-                        break;
-
-                }
-            }while (opcion!=4);
-
-
-
+//            String[] opciones = new String[]{"Menu paciente", "Menu medicos","Menu Administrador","Menu Personal Administrativo","Salir"};
+//            int opcion;
+//            do {
+//                opcion = JOptionPane.showOptionDialog(null,
+//                        "Seleccione una opcion:",
+//                        "Menu principal",
+//                        0,
+//                        0,
+//                        null,
+//                        opciones,
+//                        opciones);
+//                switch (opcion) {
+//                    case 0:
+//                        paciente1.menuPaciente();
+//                        break;
+//                    case 1:
+//                        medico1.menuMedico();
+//                        break;
+//                    case 2:
+//                        admin.menuadministrador();
+//                        break;
+//                    case 3:
+//                        personal.menuPersonalAdministrativo();
+//                        break;
+//                    case 4:
+//                        JOptionPane.showMessageDialog(null, "Saliendo...");
+//                        break;
+//
+//                }
+//            }while (opcion!=4);
+//
+//
+//
 
 
     }
