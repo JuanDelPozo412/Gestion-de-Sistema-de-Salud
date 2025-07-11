d8528180e147616b3801c6fc624b38481b5fe0ee
Juan Del Pozo <juandelpozo412@gmail.com>
2025-07-11 20:27:21 -0300

    Simplificacion del main

--- Github repository/src/GUI/Main.java
+++ Github repository/src/GUI/Main.java
@@ -9,107 +9,24 @@ import java.sql.Date;
 
 public class Main {
     public static void main(String[] args) {
-        String opciones[] = {"Login", "Register", "Salir"};
-        boolean flag = true;
-        int opcion;
-        int opcionregister;
-        int seleccion;
-        String opcionesRegister[] = {"administrador", "medico", "paciente"};
 
-        do {
-            opcion = JOptionPane.showOptionDialog(
-                    null,
-                    "Seleccione una opcion:",
-                    "Menu de Medico",
-                    0,
-                    0,
-                    null,
-                    opciones,
-                    opciones[0]
-            );
-
-            switch (opcion) {
-                case 0:
-                    String nombre = JOptionPane.showInputDialog(null, "Ingresa tu nombre de usuario");
-                    String contrasenia = JOptionPane.showInputDialog(null, "Ingresa tu contrasenia");
-
-                    Usuario u = Usuario.login(nombre, contrasenia);
-
-                    if (u instanceof Paciente) {
-                        ((Paciente) u).mostrarMenuPaciente();
-                    } else if (u instanceof Medico) {
-                        ((Medico) u).menuMedico();
-                    } else if (u instanceof Administrador) {
-                        ((Administrador) u).mostrarMenuAdmin();
-                    } else {
-                        System.out.println("BLL.Usuario devuelto es nulo");
-                    }
-                    break;
-
-                case 1:
-                    try {
-                        String[] tipos = {"paciente", "medico", "administrador"};
-                        String tipo = (String) JOptionPane.showInputDialog(
-                                null,
-                                "Seleccione tipo de usuario:",
-                                "Registro",
-                                JOptionPane.QUESTION_MESSAGE,
-                                null,
-                                tipos,
-                                tipos[0]
-                        );
+        //este trycatch maneja una exepcion cuando se aplica el "lookandfeel" sirve para q la vista tome el estilo que viene en el sistema operativo lo hace ver mas lindo, lo dejo comentado por si lo quieren aplicar, de momento voy con lo basico
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
-                        String nombreRegistro = JOptionPane.showInputDialog("Ingrese nombre:");
-                        String apellidoRegistro = JOptionPane.showInputDialog("Ingrese apellido:");
-                        String mailRegistro = JOptionPane.showInputDialog("Ingrese mail:");
-                        String dniRegistro = JOptionPane.showInputDialog("Ingrese DNI:");
-                        String contraseniaRegistro = JOptionPane.showInputDialog("Ingrese contraseña:");
-                        Date fechaNacimientoRegistro = Date.valueOf(JOptionPane.showInputDialog("Ingrese fecha de nacimiento (YYYY-MM-DD):"));
+        //Swing Utilities lo que hace es ejecutar la pantalla cuando sea seguro, es basicamente para evitar bugs, todavia no lo entiendo bien igualmente
+//        SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                new PantallaHome().setVisible(true);
+//            }
+//        });
 
-                        switch (tipo) {
-                            case "paciente":
-                                String[] planes = {"1 - Básico", "2 - Premium", "3 - Avanzado"};
-                                String seleccionado = (String) JOptionPane.showInputDialog(
-                                        null, "Seleccione plan de salud:", "Planes",
-                                        JOptionPane.QUESTION_MESSAGE, null, planes, planes[0]);
-                                int planId = Integer.parseInt(seleccionado.split(" ")[0]);
+        //por eso lo dejo de la forma simple
+        new PantallaHome().setVisible(true);
 
-                                Paciente paciente = new Paciente(
-                                        0, nombreRegistro, apellidoRegistro, mailRegistro, dniRegistro, contraseniaRegistro,
-                                        fechaNacimientoRegistro, tipo, null, null, planId
-                                );
-                                Usuario.RegistrarUsuario(paciente);
-                                break;
-                            case "medico":
-                                String especialidad = JOptionPane.showInputDialog("Ingrese especialidad:");
-                                Medico medico = new Medico(
-                                        0, nombreRegistro, apellidoRegistro, mailRegistro, dniRegistro, contraseniaRegistro,
-                                        fechaNacimientoRegistro, tipo, especialidad
-                                );
-                                Usuario.RegistrarUsuario(medico);
-                                break;
-
-                            case "administrador":
-                                String cargo = JOptionPane.showInputDialog("Ingrese cargo:");
-                                Administrador admin = new Administrador(
-                                        0, nombreRegistro, apellidoRegistro, mailRegistro, dniRegistro, contraseniaRegistro,
-                                        fechaNacimientoRegistro, tipo, cargo
-                                );
-                                Usuario.RegistrarUsuario(admin);
-                                break;
-                        }
-
-                    } catch (Exception e) {
-                        JOptionPane.showMessageDialog(null, "error de registro" + e.getMessage());
-                    }
-                case 2:
-
-                    JOptionPane.showMessageDialog(null, "Adios masterrr");
-                    flag = false;
-                    break;
-            }
-
-        } while (flag);
     }
 }
